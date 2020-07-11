3706d7017d4386203fb9c91be57803171ff70788
creendev <developer@creen.io>
2020-07-11 13:39:04 +0200

    All use cases completed

--- js/main.js
+++ js/main.js
@@ -23,12 +23,29 @@ form.addEventListener('submit', (e) => {
 
     if (validationPassed) {
         // TODO: set interval for loading spinner, after that alert user that his account had been successfully registered
+        // $('#overlay').fadeIn().delay(2000).fadeOut(); // jQuery solution
+        let overlay = document.getElementById('overlay')
+        this.fade('in', 500, overlay);
+        //this.fade('out', 5000, overlay);
+        window.setTimeout(registration_finished, 3000);
+
+
+        function registration_finished() {
+            this.fade('out', 500, overlay);
+            // TODO: alert user registration had finished
+            Swal.fire({
+                title: 'Account registered!',
+                text: 'You have successfully registered your Citrus Systems account',
+                icon: 'success',
+                confirmButtonText: 'Proceed to login'
+            })
+        }
+
     }
 
 })
 
 function validateEmail(mail) {
-    console.log("validate email invoked");
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
         return (true)
     }
@@ -42,15 +59,47 @@ function validateEmail(mail) {
 }
 
 function validatePhone(phone) {
-    console.log("validate phone invoked");
-    if (/^\d{10}$/.test(phone)) {
+    if (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(phone)) {
         return (true)
     }
     Swal.fire({
         title: 'Error!',
-        text: 'You have entered an invalid mobile number!',
+        html: 'You have entered an invalid mobile number! <br>' +
+            'Supported mobile phone formats are: <br>' +
+            '(123) 456-7890 <br>' +
+            '(123)456-7890 <br>' +
+            '123-456-7890 <br>' +
+            '123.456.7890 <br>' +
+            '1234567890 <br>' +
+            '+38112345678 <br>' +
+            '062-1636188',
         icon: 'error',
         confirmButtonText: 'Close'
     })
     return (false)
+}
+
+
+function fade(type, ms, el) {
+    var isIn = type === 'in',
+        opacity = isIn ? 0 : 1,
+        interval = 50,
+        duration = ms,
+        gap = interval / duration;
+
+    if (isIn) {
+        el.style.display = 'inline';
+        el.style.opacity = opacity;
+    }
+
+    function func() {
+        opacity = isIn ? opacity + gap : opacity - gap;
+        el.style.opacity = opacity;
+
+        if (opacity <= 0) el.style.display = 'none'
+        if (opacity <= 0 || opacity >= 1) window.clearInterval(fading);
+    }
+
+    var fading = window.setInterval(func, interval);
+
 }
