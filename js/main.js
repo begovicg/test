be636fe465c2dbd8407cfc35dd0ea26b4c267479
creendev <developer@creen.io>
2020-07-11 11:41:02 +0200

    Refactored registration form, reading values from form in main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
-const email_form = document.getElementById('email_form')
-const mobile_form = document.getElementById('mobile_form')
+const form = document.getElementById('form')
 
-email_form.addEventListener('submit', (e) => {
-    console.log("submit invoked for: ", e);
+const email_reg_tab = document.getElementById('email_reg-tab')
+const mobile_reg_tab = document.getElementById('mobile_reg-tab')
+
+const email = document.getElementById('email')
+const mobile = document.getElementById('mobile')
+const currency = document.getElementById('currency')
+const terms = document.getElementById('terms')
+const notifications = document.getElementById('notifications')
+
+
+form.addEventListener('submit', (e) => {
+
     e.preventDefault();
-})
+
+    console.log("email_reg_tab: ", email_reg_tab.getAttribute('aria-selected'));
+    console.log("mobile_reg_tab: ", mobile_reg_tab.getAttribute('aria-selected'));
+
+    console.log("email value: ", email.value);
+    console.log("mobile value: ", mobile.value);
+    console.log("currency value: ", currency.value);
+    console.log("terms checked: ", terms.checked);
+    console.log("notifications checked: ", notifications.checked);
 
 
-mobile_form.addEventListener('submit', (e) => {
-    console.log("submit invoked for: ", e);
-    e.preventDefault();
 })
